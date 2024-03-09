f8464dd8b9c489c8e06a248826a1d3167010fc03
chetan <chetankolhe72@gmail.com>
2024-03-09 12:45:40 +0530

    Added test scenario

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/PylinuxTech/go_lang_testing
+
+go 1.18
---
+++ mathoperation/operation.go
@@ -0,0 +1,13 @@
+package mathoperation
+
+func Add(a, b int) int {
+	return a + b
+}
+
+func Sub(a, b int) int {
+	return a - b
+}
+
+func Mult(a, b int) int {
+	return a * b
+}
